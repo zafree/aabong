b78c6ed8724e415efc820348642d8ae8dd5281d9
Foysal Zafree <zafree@deligram.com>
2019-12-26 18:15:15 +0600

    assets

---
+++ assets/scripts/main.js
@@ -0,0 +1,24 @@
+/**
+ * Zafree v1.0.0 - The story of us
+ * @link https://zafree.github.io/aabong
+ * @copyright 2019-2019 Zafree
+ * @license 
+ */
+"use strict";
+
+;
+
+(function () {
+  'use strict';
+
+  console.log('Hello');
+})();
+"use strict";
+
+;
+
+(function () {
+  'use strict';
+
+  console.log('World!');
+})();
